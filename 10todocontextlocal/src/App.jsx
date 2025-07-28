90df34b862f4657cad42c85fc0fb5e11f75f203e
Prateek undead <pr805201@gmail.com>
2025-07-28 12:39:59 +0530

    starting todo

---
+++ 10todocontextlocal/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="p-1 bg-yellow-200 text-2xl"> testing tailwind </h1>
+    </>
+  );
+}
+
+export default App;
