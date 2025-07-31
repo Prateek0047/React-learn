a8e3cd94fcdcf5031211114ea5bb22d32a345493
Prateek undead <pr805201@gmail.com>
2025-07-31 14:54:49 +0530

    finishing todo using react-redux

--- reduxToolkit/src/App.jsx
+++ reduxToolkit/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
 import "./App.css";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1> learing about redux toolkit</h1>
+      <h1 className="bg-red-200 text-4xl"> learning about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
